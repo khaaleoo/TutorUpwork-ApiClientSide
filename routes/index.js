a702de96036811b1ed9cdc343d1bd3c742493c46
Kha Leo <lexuankha2409@gmail.com>
2019-11-20 15:51:02 +0700

    init project by express-generator

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log("Listening on port 8000");
+  res.send("ApiClientSide");
+});
+
+module.exports = router;
