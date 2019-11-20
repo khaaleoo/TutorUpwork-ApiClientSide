dedd4718dd85645025f7d49f596307e9b19dc515
tdkhai1998 <trandinhkhai10.1@gmail.com>
2019-11-20 21:08:46 +0700

    khai: add eslint and husky

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res) {
   console.log("Listening on port 8000");
   res.send("ApiClientSide");
 });
