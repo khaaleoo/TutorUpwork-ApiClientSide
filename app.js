b549a850c8dcdf8d7d8ae310843516e728f4210e
tdkhai1998 <trandinhkhai10.1@gmail.com>
2019-11-20 21:47:01 +0700

    config eslint

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require("express");
+const express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./src/index").default;
-var usersRouter = require("./src/index").default;
+var usersRouter = require("./src/index/d").default;
 
 var app = express();
 
@@ -12,7 +12,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+require("dsfg");
 /* GET home page. */
 router.get("/", function(req, res) {
   console.log("Listening on port 8000");
