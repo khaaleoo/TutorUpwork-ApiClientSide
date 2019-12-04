2e27a6b713849120caa553c0abc2f4aeae1da67d
tdkhai1998 <trandinhkhai10.1@gmail.com>
2019-12-04 08:38:45 +0700

    intergrate login local and register

--- src/auth/passport.ts
+++ src/auth/passport.ts
@@ -1,21 +1,28 @@
 import passport from "passport";
 import passportLocal from "passport-local";
 import { UserModel } from "../user/user.model";
-import { compare } from "bcrypt"
 import passportJwt, { ExtractJwt } from "passport-jwt";
-
+var User = require('mongoose').model('User');
 const LocalStrategy = passportLocal.Strategy;
 const JwtStrategy = passportJwt.Strategy;
+import { compare } from "bcrypt"
 
 const local = new LocalStrategy(
     { usernameField: "email" },
     async (email, password, done) => {
-        const userList = await UserModel.find({ username: email });
-        if (userList.length === 0) return done(null, false, { message: "Invalid username." });
-        const user = userList[0];
-        let ret = await user.comparePassword(password, user.password);
-        if (ret) done(null, user);
-        else done(null, false, { message: "Invalid password." });
+        try {
+            console.log(email, password)
+            const userList = await UserModel.find({ email: email });
+            console.log(userList)
+            if (userList.length === 0) return done(null, false, { message: "Invalid username." });
+            const user = userList[0];
+            let ret = await compare(password, user.password);
+            console.log("ret", ret)
+            if (ret) done(null, user);
+            else done(null, false, { message: "Invalid password." });
+        } catch (err) {
+            done(err);
+        }
     }
 )
 
@@ -25,13 +32,12 @@ const jwt = new JwtStrategy(
         secretOrKey: "123456"
     },
     async (jwtToken, done) => {
-        const userList = await UserModel.find({ username: jwtToken.username });
+        const userList = await UserModel.find({ email: jwtToken.email });
         if (userList.length === 0) return done(undefined, false);
         return done(undefined, userList[0], jwtToken);
     }
 )
-
 passport.use(jwt);
-passport.use(local)
+passport.use(local);
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
--- src/user/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAll, getOne } from "./user.controller"
-
-const router = Router();
-
-router.get("/", getAll);
-router.get("/:id", getOne)
-
-export default router;
