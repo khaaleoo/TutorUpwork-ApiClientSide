8e41d9593c6b200baefb9a71ba02d1e52879837c
wedday2004 <48178473+wedday2004@users.noreply.github.com>
2019-12-11 18:17:28 +0700

    filter all

--- src/tutor/tutor.controller.ts
+++ src/tutor/tutor.controller.ts
@@ -1,21 +1,24 @@
 import { NextFunction, Request, Response } from "express";
-import { TutorModel, Tutor } from './tutor.model';
+import { TutorModel, Tutor } from "./tutor.model";
 import { plainToClass } from "class-transformer";
 
 export class TutorController {
   public updateOne(req: any, res: Response) {
     const { body } = req;
     console.log(body);
-    console.log(req.user.id)
-    TutorModel.update({ id: req.user.id }, {
-      $set: {
-        ...body
+    console.log(req.user.id);
+    TutorModel.update(
+      { id: req.user.id },
+      {
+        $set: {
+          ...body
+        }
+      },
+      (err, raw) => {
+        console.log("result", err, raw);
+        res.status(200).json("sdfsd");
       }
-    }, (err, raw) => {
-      console.log("result", err, raw);
-      res.status(200).json("sdfsd");
-    }
-    )
+    );
   }
   public async getAll(req: Request, res: Response): Promise<void> {
     const result = await TutorModel.find({});
@@ -26,24 +29,28 @@ export class TutorController {
   }
   public async getByFilters(req: Request, res: Response): Promise<void> {
     const filter = req.body;
-    const param = [];
-    // { skills: { $all: ["HTML", "CSS"] } },
-
-    if (filter.city) {
-      param.push(`{"address.city":${filter.city}}`);
+    let skill = {};
+    if (filter.skills && filter.skills.length !== 0) {
+      skill = { skills: { $all: filter.skills } };
     }
-    if (filter.district) {
-      param.push(`{"address.district":${filter.district}}`);
+    let city = {};
+    if (filter.city !== false) {
+      city = { "address.city": filter.city };
+      console.log(filter.city);
     }
-    if (filter.skills) {
-      param.push(`skills: { $all: ${filter.skills}}`);
+    let district = {};
+    if (filter.district !== false) {
+      district = { "address.district": filter.district };
     }
 
     console.log(filter.price);
     const result = await TutorModel.find({
       $and: [
         { price: { $gt: filter.price[0] } },
-        { price: { $lt: filter.price[1] } }
+        { price: { $lt: filter.price[1] } },
+        { ...skill },
+        { ...city },
+        { ...district }
       ]
     });
     res.status(200).send({
