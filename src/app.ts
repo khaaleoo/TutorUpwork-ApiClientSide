14fddecda2305ef968dae118e6bd5e6b851ae185
wedday2004 <48178473+wedday2004@users.noreply.github.com>
2019-12-26 09:43:49 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -102,7 +102,10 @@ class Server {
   public config(): void {
     let allowCrossDomain = function(_req: any, res: any, next: any) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "*, Origin, X-Requested-With, Content-Type, Accept, secret_token"
+      );
       res.header(
         "Access-Control-Allow-Methods",
         "GET, POST, OPTIONS, PUT,    PATCH, DELETE"
